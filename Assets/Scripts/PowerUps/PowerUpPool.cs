aa868663724e104cb40ee34e85d69f6ec3faf3d1
Yashvardhan4197 <20BCS4197@cuchd.in>
2024-08-23 10:43:17 +0530

    Created generic SubType for PowerUpPool

---
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -0,0 +1,32 @@
+using System;
+using CosmicCuration.Utilities;
+
+namespace CosmicCuration.PowerUps
+{
+    public class PowerUpPool: GenericObjectPool<PowerUpController>
+    {
+        private PowerUpData powerUpData;
+        public PowerUpController GetPowerUp<T>(PowerUpData powerUpData) where T : PowerUpController
+        {
+            this.powerUpData = powerUpData;
+            return GetItem<T>();
+        }
+
+        protected override PowerUpController CreateItem<T>()
+        {
+            if(typeof(T)==typeof(Shield))
+            {
+                return new Shield(powerUpData);
+            }
+            else if (typeof(T) == typeof(RapidFire))
+            {
+                return new RapidFire(powerUpData);
+            }
+            else if (typeof(T) == typeof(DoubleTurret))
+            {
+                return new DoubleTurret(powerUpData);
+            }
+            throw new NotSupportedException("UnAuthorized Class");
+        }
+    }
+}
